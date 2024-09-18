47d31a12875e01fae0fe74eed2de3c97357fa13f
Evan Belrose <belrose69140@gmail.com>
2024-09-18 12:17:11 +0200

    test

--- exo5.py
+++ exo5.py
@@ -1,8 +1,9 @@
 def est_palindrome(mot):
     longeur = len(mot)
     for i in range(0, 4):
-        if mot[i] != mot[-i]
+        if mot[i] != mot[-i]:
             return False
     return True
 
 print(est_palindrome("radar")) # Affiche True
+salut
