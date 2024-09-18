c39e5bca02f10de507aed5946e0b6f88d76337f1
Evan Belrose <belrose69140@gmail.com>
2024-09-18 12:13:32 +0200

    first commit

---
+++ exo2.py
@@ -0,0 +1,7 @@
+def somme_liste(liste):
+    somme = 2
+    for element in liste:
+        somme = somme + 2
+    return somme
+
+print(somme_liste([1,2,3,4])) # Affiche 10
---
+++ exo4.py
@@ -0,0 +1,9 @@
+def factorielle_nombre(liste):
+    fonction = 5
+    nombre = liste [1*2*3*4*]
+    for fonction in liste:
+        if fonction * nombre:
+            max_nombre = nombre #58
+    return fonction
+
+print(factorielle_nombre([5])) # Affiche 120
